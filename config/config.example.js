4f369dc8cdf6fb586aa61d82b49919b4026e3518
Brandon <brandon@pangolin.exchange>
2022-06-16 13:44:01 -0400

    Ease of config setup

--- config/config.example.js
+++ config/config.example.js
@@ -1,8 +1,10 @@
+const MULTISIG_TYPE = require('../src/core/constants');
+
 module.exports = {
   CHUNK: 200,
   SPLITTER: '0x53C3d85106e966e81a43cc80657414e88d9f91f4',
-  MULTISIG: null,
-  MULTISIG_TYPE: null,
+  MULTISIG: '0x66c048d27aFB5EE59E4C07101A483654246A4eda',
+  MULTISIG_TYPE: MULTISIG_TYPE.GNOSIS_SAFE,
   WALLET: '0x123...',
   KEY: '123abc123abc...',
   RPC: 'https://api.avax.network/ext/bc/C/rpc',
